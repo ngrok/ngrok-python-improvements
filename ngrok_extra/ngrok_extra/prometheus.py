4e57833a868f6c4a1cbb8e4650e9d5b0565406dd
James Szalay <james@ngrok.com>
2024-03-01 21:16:41 +0000

    runs make black

--- ngrok_extra/ngrok_extra/prometheus.py
+++ ngrok_extra/ngrok_extra/prometheus.py
@@ -5,23 +5,30 @@ import logging
 import random
 import string
 
+
 class BasicAuth(t.NamedTuple):
     username: str
     password: str
 
 
-def start_http_server(port: int = 8000, authtoken: str = None, domain: str = None, basic_auth: BasicAuth = None, policy: str = None) -> t.Tuple[ngrok.Listener, t.Callable[[], None]]:
+def start_http_server(
+    port: int = 8000,
+    authtoken: str = None,
+    domain: str = None,
+    basic_auth: BasicAuth = None,
+    policy: str = None,
+) -> t.Tuple[ngrok.Listener, t.Callable[[], None]]:
     """
     Starts the prometheus default HTTP exporter server AND an ngrok endpoint forwarding traffic to it, with optional static domain and basic auth protection.
-    
+
     Keyword arguments:
     port - required. local port for the prometheus builting http server to listen on
     authtoken - optional. authtoken to use. Reads from NGROK_AUTHTOKEN environment variable if omitted. An auth token must be supplied by one of these methods.
-    domain - optional but necessary for non-testing use. static domain to use. if not supplied, your external ngrok url will change each time you restart. 
+    domain - optional but necessary for non-testing use. static domain to use. if not supplied, your external ngrok url will change each time you restart.
     basic_auth - optional. adds basic auth protection if supplied, this is supported out-of-the-box by prometheus scrape configs.
     policy - optional. policy+action configuration json. See ngrok.policy for builder API.
 
-    Returns: 
+    Returns:
       listener - an ngrok.Listener for the created listener
       shutdown() - a function that can be used to gracefully shut down the ngrok listener and the prometheus HTTP server.
     """
@@ -31,11 +38,12 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
     else:
         kwargs["authtoken_from_env"] = True
 
-    if domain: 
+    if domain:
         kwargs["domain"] = domain
     else:
-        logging.warning("Since domain= was omitted, starting with a random ngrok domain. This will change each time you restart the program.")
-
+        logging.warning(
+            "Since domain= was omitted, starting with a random ngrok domain. This will change each time you restart the program."
+        )
 
     auth_is_generated: bool
     if basic_auth:
@@ -43,9 +51,13 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
         auth_is_generated = False
     else:
         auth_is_generated = True
-        random_pw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        random_pw = "".join(
+            random.choices(string.ascii_uppercase + string.digits, k=10)
+        )
         basic_auth = BasicAuth("root", random_pw)
-        logging.warning(f"Since basic_auth= was omitted, starting with a random password. This will change each time you restart. The username is \"root\", the password is: {random_pw}")
+        logging.warning(
+            f'Since basic_auth= was omitted, starting with a random password. This will change each time you restart. The username is "root", the password is: {random_pw}'
+        )
         kwargs["basic_auth"] = f"root:{random_pw}"
 
     if policy:
@@ -53,7 +65,8 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
 
     listener = ngrok.forward(port, **kwargs)
 
-    logging.info(f"""
+    logging.info(
+        f"""
         Prometheus scrape config:
         - job_name: 'my_prometheus_exporter_with_ngrok'
           scrape_interval: 5m
@@ -64,7 +77,8 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
           basic_auth:
               username: '{basic_auth.username}'
               password: '{basic_auth.password if auth_is_generated else "redacted"}'
-""")
+"""
+    )
 
     server, thread = prometheus_client.start_http_server(port)
 
@@ -72,7 +86,5 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
         ngrok.disconnect(listener.url())
         server.shutdown()
         thread.join()
-    
+
     return listener, shutdown
-
-
--- ngrok_extra/ngrok_extra/test_prometheus.py
+++ ngrok_extra/ngrok_extra/test_prometheus.py
@@ -2,13 +2,19 @@ from ngrok_extra.prometheus import start_http_server, BasicAuth
 import unittest
 import time
 
+
 class TestNgrokUtils(unittest.TestCase):
     def test_prom_server_setup(self):
-        listener, shutdown = start_http_server(8000, domain="joshtestprom.ngrok.dev", basic_auth=BasicAuth("josh", "31948ru98yhr4fre7wfy6t"))
+        listener, shutdown = start_http_server(
+            8000,
+            domain="joshtestprom.ngrok.dev",
+            basic_auth=BasicAuth("josh", "31948ru98yhr4fre7wfy6t"),
+        )
         print(listener.url())
         time.sleep(10)
         shutdown()
         print("done")
+
 
 if __name__ == "__main__":
     unittest.main()
