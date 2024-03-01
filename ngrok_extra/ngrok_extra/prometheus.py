e079ac17c2661c0f084d9e4c784e74cf289f0a17
Josh Hertlein <jmh@ngrok.com>
2024-03-01 19:32:14 +0000

    print example prom config to logging.info

--- ngrok_extra/ngrok_extra/prometheus.py
+++ ngrok_extra/ngrok_extra/prometheus.py
@@ -37,18 +37,34 @@ def start_http_server(port: int = 8000, authtoken: str = None, domain: str = Non
         logging.warning("Since domain= was omitted, starting with a random ngrok domain. This will change each time you restart the program.")
 
 
+    auth_is_generated: bool
     if basic_auth:
         kwargs["basic_auth"] = f"{basic_auth.username}:{basic_auth.password}"
+        auth_is_generated = False
     else:
+        auth_is_generated = True
         random_pw = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+        basic_auth = BasicAuth("root", random_pw)
         logging.warning(f"Since basic_auth= was omitted, starting with a random password. This will change each time you restart. The username is \"root\", the password is: {random_pw}")
         kwargs["basic_auth"] = f"root:{random_pw}"
 
     if policy:
         kwargs["policy"] = policy
 
-    
     listener = ngrok.forward(port, **kwargs)
+
+    logging.info(f"""
+        Prometheus scrape config:
+        - job_name: 'my_prometheus_exporter_with_ngrok'
+          scrape_interval: 5m
+          scrape_timeout: 30s
+          static_configs:
+              - targets: ['{listener.url()}']
+          metrics_path: "/metrics"
+          basic_auth:
+              username: '{basic_auth.username}'
+              password: '{basic_auth.password if auth_is_generated else "redacted"}'
+""")
 
     server, thread = prometheus_client.start_http_server(port)
 
